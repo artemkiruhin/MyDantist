1e7cfa36cd664138f6a12b59286583ed96706d7c
glamfuneral_laptop <temkaunik@gmail.com>
2024-09-30 12:32:18 +0300

    DbContext Created & Treatment upd

---
+++ Infrastructure/Database/DantistClinicDbContext.cs
@@ -0,0 +1,92 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Database
+{
+    public class DantistClinicDbContext : DbContext
+    {
+        protected DantistClinicDbContext() : base()
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Treatment> Treatments { get; set; }
+
+        public DantistClinicDbContext(DbContextOptions<DantistClinicDbContext> options)
+           : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<Employee>().HasKey(e => e.Id);
+            modelBuilder.Entity<Patient>().HasKey(p => p.Id);
+            modelBuilder.Entity<Appointment>().HasKey(a => a.Id);
+            modelBuilder.Entity<Treatment>().HasKey(t => t.Id);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Role)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Birthday)
+                .HasConversion<DateOnlyConverter, DateOnlyComparer>();
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.HireDate)
+                .HasConversion<DateOnlyConverter, DateOnlyComparer>();
+
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.User)
+                .WithOne()
+                .HasForeignKey<Employee>(e => e.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Patient>()
+                .HasOne(p => p.User)
+                .WithOne()
+                .HasForeignKey<Patient>(p => p.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Patient)
+                .WithMany(p => p.Appointments)
+                .HasForeignKey(a => a.PatientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Dentist)
+                .WithMany()
+                .HasForeignKey(a => a.DentistId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Treatment>()
+                .HasOne(t => t.Appointment)
+                .WithMany()
+                .HasForeignKey(t => t.AppointmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+
+    public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
+    {
+        public DateOnlyConverter() : base(
+            d => d.ToDateTime(TimeOnly.MinValue),
+            d => DateOnly.FromDateTime(d))
+        { }
+    }
+
+    public class DateOnlyComparer : ValueComparer<DateOnly>
+    {
+        public DateOnlyComparer() : base(
+            (d1, d2) => d1.DayNumber == d2.DayNumber,
+            d => d.GetHashCode())
+        { }
+    }
+}
