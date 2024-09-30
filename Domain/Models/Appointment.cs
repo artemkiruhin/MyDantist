415cb7af4f41da36dce6d3852260a2ef2971c6c8
ArtemPC <temkaunik@gmail.com>
2024-09-30 10:06:26 +0300

    Patient & appointment created

---
+++ Domain/Models/Appointment.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain.Models
+{
+    public class Appointment
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid PatientId { get; set; }
+        public Patient? Patient { get; set; }
+        public Guid DentistId { get; set; }
+        public Employee? Dentist { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+        public string? Purpose { get; set; }
+        public string? Notes { get; set; }
+
+        public static Appointment Create(Guid id, Guid patientId, Patient? patient, Guid dentistId, Employee? dentist, DateTime appointmentDateTime, string? purpose, string? notes)
+        {
+            return new Appointment()
+            {
+                Id = id,
+                PatientId = patientId,
+                Patient = patient,
+                DentistId = dentistId,
+                Dentist = dentist,
+                AppointmentDateTime = appointmentDateTime,
+                Purpose = purpose,
+                Notes = notes
+            };
+        }
+    }
+}
---
+++ Domain/Models/Patient.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Models
+{
+    public class Patient
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+        public string? MedicalHistory { get; set; }
+        public string? AllergiesAndReactions { get; set; }
+        public List<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+        public static Patient Create(Guid id, Guid userId, User? user, string? medicalHistory, string? allergiesAndReactions, List<Appointment> appointments)
+        {
+            return new Patient()
+            {
+                Id = id,
+                UserId = userId,
+                User = user,
+                MedicalHistory = medicalHistory,
+                AllergiesAndReactions = allergiesAndReactions,
+                Appointments = appointments
+            };
+        }
+    }
+}
