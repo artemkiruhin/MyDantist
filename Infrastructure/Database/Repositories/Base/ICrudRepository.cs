f60b7b2bbdf41838ae91dc108c61e45424e2b155
glamfuneral_laptop <temkaunik@gmail.com>
2024-09-30 13:52:24 +0300

    ICrudRepository created

---
+++ Infrastructure/Database/Repositories/Base/ICrudRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Database.Repositories.Base
+{
+    public interface ICrudRepository<T>
+    {
+        Task<T?> GetById(Guid id);
+        Task<IEnumerable<T>> GetAll();
+        Task<bool> Add(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(Guid id);
+    }
+}
